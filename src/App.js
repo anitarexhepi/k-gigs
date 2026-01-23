f2098d5f5f8c124a1c2901cba375c6e9945ed68d
Nekibe-Bytyqi <nekibebytyqi@gmail.com>
2026-01-23 03:04:40 -0800

    Ndryshimet e nevojshme ne App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import AboutUs from "./components/AboutUs";
 import HomePage from "./components/HomePage";
 import Layout from './components/Layout';
 import ContactUs from "./components/ContactUs";
+
 import SignUp from './components/SignUp';
 import Login from './components/Login';
-
+import PostGig from "./components/PostGig";
+import PunedhenesDashboard from "./components/PunedhenesDashboard";
+import ProtectedPunedhenesRoute from "./components/ProtectedPunedhenesRoute";
 
 function App() {
   return (
@@ -16,9 +19,28 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<ContactUs />} />
+       
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-         
+
+          {/* PUNEDHENES ONLY */}
+          <Route
+            path="/post-gig"
+            element={
+              <ProtectedPunedhenesRoute>
+                <PostGig />
+              </ProtectedPunedhenesRoute>
+            }
+          />
+
+          <Route
+            path="/punedhenes-dashboard"
+            element={
+              <ProtectedPunedhenesRoute>
+                <PunedhenesDashboard />
+              </ProtectedPunedhenesRoute>
+            }
+          />
         </Routes>
       </Layout>
     </Router>
@@ -26,3 +48,4 @@ function App() {
 }
 
 export default App;
+
