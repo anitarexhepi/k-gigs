e9f3c4b3c88a0b74cafce2024bf4b46247803caf
Nekibe-Bytyqi <nekibebytyqi@gmail.com>
2026-01-23 02:50:58 -0800

    Inicializo konfigurimin e databazës dhe .env

---
+++ backend/src/config/db.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2/promise');
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require('./env');
+
+
+const pool = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  
+});
+
+
+pool.getConnection()
+  .then(conn => {
+    console.log('✅ MySQL Connected');
+    conn.release(); 
+  })
+  .catch(err => {
+    console.error('❌ MySQL Connection Error:', err);
+  });
+
+module.exports = pool;
